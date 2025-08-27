fbfabd351c79a657bc18da0394f94378e460ae5e
hidehic0 <hidehico.ooba@gmail.com>
2025-08-27 20:47:28 +0900

    feat(unionfind): 2D unionfindを追加(なお内部構造で普通のunionfindを使っているだけ)

--- data-structure/unionfind.hpp
+++ data-structure/unionfind.hpp
@@ -33,3 +33,26 @@ struct UnionFind {
 
   bool same(int a, int b) { return leader(a) == leader(b); }
 };
+
+struct UnionFind2D {
+public:
+  int _h, _w;
+  UnionFind internal_uf;
+
+  UnionFind2D(int h, int w) : _h(h), _w(w), internal_uf(h * w) {}
+
+  int leader(int h, int w) { return internal_uf.leader(get_id(h, w)); };
+  bool merge(int ah, int aw, int bh, int bw) {
+    return internal_uf.merge(get_id(ah, aw), get_id(bh, bw));
+  }
+  int size(int h, int w) { return internal_uf.size(get_id(h, w)); }
+  bool same(int ah, int aw, int bh, int bw) {
+    return internal_uf.same(get_id(ah, aw), get_id(bh, bw));
+  }
+
+private:
+  int get_id(int h, int w) {
+    assert(0 <= h && 0 < _h && 0 <= w && w < _w);
+    return h * _w + w;
+  }
+};
