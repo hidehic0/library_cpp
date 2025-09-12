a3dffa0638d23393896fe31338f87bb9d7aaee75
hidehic0 <hidehico.ooba@gmail.com>
2025-09-12 12:06:34 +0900

    feat: 順番を反転させるrepマクロを追加

--- templates/macro.hpp
+++ templates/macro.hpp
@@ -1,8 +1,10 @@
 #pragma once
 #include <iostream>
 
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define REP(i, a, b) for (ll i = (int)(a); i < (int)(b); ++i)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rrep(i, n) for (int = n - 1; i >= 0; i--)
+#define REP(i, a, b) for (int i = (int)(a); i < (int)(b); ++i)
+#define RREP(i, a, b) for (int i = (int)(b - 1); i >= (int)(a); i--)
 #define all(a) (a).begin(), (a).end()
 
 template <typename T> bool chmin(T &a, T b) {
