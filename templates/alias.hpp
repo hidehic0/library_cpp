e372e086d40874fa149e7cae43892cdb7db538e5
hidehic0 <hidehico.ooba@gmail.com>
2025-09-20 11:06:36 +0900

    feat: pair<long long, long long>の型aliasを追加

--- templates/alias.hpp
+++ templates/alias.hpp
@@ -10,6 +10,7 @@ using vi = std::vector<long long>;
 using vvi = std::vector<std::vector<long long>>;
 using si = std::set<long long>;
 using mii = std::map<long long, long, long>;
+using pii = std::pair<long long, long long>;
 template <typename T> using V = std::vector<T>;
 const std::string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const std::string lowerlist = "abcdefghijklmnopqrstuvwxyz";
