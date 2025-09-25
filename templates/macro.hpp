0670d4a1c6e687783f94ea15f8b718bc2bc61c58
hidehic0 <hidehico.ooba@gmail.com>
2025-09-25 19:48:48 +0900

    fix(macro): 構文エラーになるdefineがあったので修正

--- templates/macro.hpp
+++ templates/macro.hpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define rrep(i, n) for (int = n - 1; i >= 0; i--)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
 #define REP(i, a, b) for (int i = (int)(a); i < (int)(b); ++i)
 #define RREP(i, a, b) for (int i = (int)(b - 1); i >= (int)(a); i--)
 #define all(a) (a).begin(), (a).end()
