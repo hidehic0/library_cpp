5f01396aeee8496d4ccb5a31d4356ab8bef000dd
hidehic0 <hidehico.ooba@gmail.com>
2025-10-20 12:12:46 +0900

    update(grid): グリッドの移動用配列を追加

--- grid/template.hpp
+++ grid/template.hpp
@@ -1,5 +1,9 @@
 #pragma once
+#include <bits/stdc++.h>
 
 bool coordinate_check(int x, int w, int H, int W) {
   return 0 <= x && x < H && 0 <= w && w < W;
 }
+
+constexpr std::array<std::pair<int, int>, 4> MOVES1 = {
+    {{0, 1}, {1, 0}, {-1, 0}, {0, -1}}};
