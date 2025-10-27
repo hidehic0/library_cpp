27b2e8b24ca4e668e6f3f1a8c27cc255f03ab3e3
hidehic0 <hidehico.ooba@gmail.com>
2025-10-27 20:55:37 +0900

    update(alias): set<long long>などとしていた箇所をset<ll>と修正

--- templates/alias.hpp
+++ templates/alias.hpp
@@ -6,17 +6,17 @@
 
 using ll = long long;
 using ld = long double;
-using pii = std::pair<long long, long long>;
-using vi = std::vector<long long>;
+using pii = std::pair<ll, ll>;
+using vi = std::vector<ll>;
 using vvi = std::vector<vi>;
 using vvvi = std::vector<vvi>;
 using vf = std::vector<double>;
 using vvf = std::vector<vf>;
 using vpii = std::vector<pii>;
 using vvpii = std::vector<vpii>;
-using si = std::set<long long>;
+using si = std::set<ll>;
 using spii = std::set<pii>;
-using mii = std::map<long long, long, long>;
+using mii = std::map<ll, ll>;
 const std::string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const std::string lowerlist = "abcdefghijklmnopqrstuvwxyz";
 #define mp make_pair
