14e5bb01a7e2a28f63fe2ce43b338cc5d7ce0619
hidehic0 <hidehico.ooba@gmail.com>
2026-05-20 10:34:16 +0900

    update(all.h): __has_includeを使って安全にやる

--- all.h
+++ all.h
@@ -1,4 +1,7 @@
+#if __has_include(<atcoder/all>)
 #include <atcoder/all>
+#endif
+
 #include <bits/stdc++.h>
 
 #ifdef ONLINE_JUDGE
@@ -9,9 +12,17 @@
 #define CPP_DUMP_DEFINE_EXPORT_ENUM(...)
 #define CPP_DUMP_DEFINE_EXPORT_OBJECT_GENERIC(...)
 #define export_command
-#else
+#elif __has_include(<cpp-dump/cpp-dump.hpp>)
 #include <cpp-dump/cpp-dump.hpp>
 #define dump(...) cpp_dump(__VA_ARGS__)
+#else
+#define dump(...)
+#define CPP_DUMP_SET_OPTION(...)
+#define CPP_DUMP_SET_OPTION_GLOBAL(...)
+#define CPP_DUMP_DEFINE_EXPORT_OBJECT(...)
+#define CPP_DUMP_DEFINE_EXPORT_ENUM(...)
+#define CPP_DUMP_DEFINE_EXPORT_OBJECT_GENERIC(...)
+#define export_command
 #endif
 
 #include "templates/alias.hpp"
@@ -25,3 +36,5 @@
 #include "data-structure/range_set.hpp"
 #include "data-structure/removable_priority_queue.hpp"
 #include "data-structure/unionfind.hpp"
+
+#include "matrix/matrix.hpp"
